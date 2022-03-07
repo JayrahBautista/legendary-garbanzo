a6177e1ed37a5a911f6603593409b3eca5fb8a58
JayrahBautista <201980058@psu.palawan.edu.ph>
2022-03-07 20:02:42 +0800

    done

--- web2/fav.php
+++ web2/fav.php
@@ -2,7 +2,24 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
-        
+
+        $username = isset($_POST['username']) ? $_POST['username']: '';
+        $color = isset($_POST['color']) ? $_POST['color']: '';
+        $dish = isset($_POST['dish']) ? $_POST['dish']: '';
+
+        if(!empty($username)){
+            if (!empty($color) && !empty($dish)) {
+                echo "<h1> Thank You For Your Selection, {$username} 
+                <br> You Really Enjoyed {$dish}
+                <br> - Especially With A Nice {$color} Wine. </h1>";
+            }
+            else{ 
+                echo "<h1> Dear Customer, Please Choose Your Favorite Wine and Enter Your Favorite Dish. Thank You! </h1>";
+            }
+        }
+            else{
+                echo "<h1> Dear Customer, Please Enter Your Name. Thank You!</h1>";
+        }
     ?>
 </body>
 </html>
