5c3e5eb379bd07c8f7f0c697b1279e55a9e39d89
elmerrubiojr <201980101@psu.palawan.edu.ph>
2022-03-07 20:14:19 +0800

    commit ni elmer

--- web3/calc.php
+++ web3/calc.php
@@ -1,16 +1,40 @@
 <html>
-<head><title>Calculation Result</title></head>
+<head>
+    <title>Calculation Result</title>
+    <style>
+        *{
+            text-align:center;
+            font-size:3vw;
+        }
+    </style>
+</head>
 <body>
     <?php
+        $value1=$_POST['val1'];
+        $value2=$_POST['val2'];
+        $operation=$_POST['calc'];
         # test if input is_numeric
-        if(1){
+        if(is_numeric($value1 )&& is_numeric($value2)){
             # test if calc is not null
-            if(1){
+            if(!empty($operation)){
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($operation){
+                    case "add":
+                        $result= $value1+$value2;
+                        break;
+                    case "sub":
+                        $result= $value1-$value2;
+                        break;
+                    case "mul":
+                        $result= $value1*$value2;
+                        break;
+                    case "div":
+                        $result= $value1/$value2;
+                        break;
+                    default:
+                        echo"<p>Please Try Again..</p>";
                 }
-                echo "Calculation result: $result";
+                echo "<p>Calculation result: $result</p>";
             }
         }
         else{ echo "Invalid entry - please retry"; }
